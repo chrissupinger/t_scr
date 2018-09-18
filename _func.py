67467a171fd0d2c3c09a1437180eca0818fb0eaa
Chris S <christopher.supinger@marquette.edu>
2018-09-17 23:11:23 -0500

    Add files via upload

--- _func.py
+++ _func.py
@@ -207,6 +207,8 @@ def t_scrape(origin, start, end):
 
                     except:
 
+                        _get_id = _dr.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[2]/div/div/div[2]/div/div[2]/div[4]/div/div[2]/ol[1]/li[%d]/div[1]' % start)
+                        _id = _get_id.get_attribute('data-tweet-id')
                         pass
                         print('Error processing Tweet ID {}. {} of {}.'.format(int(_id), start, end))
 
